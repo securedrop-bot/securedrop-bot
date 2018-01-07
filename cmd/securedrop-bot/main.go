86d4aed251b2cfda40ea2ae5e389f339e55fba0e
Travis Cline <travis.cline@gmail.com>
2018-01-06 23:57:09 -0500

    logger: add logrus structured logging library

--- cmd/securedrop-bot/main.go
+++ cmd/securedrop-bot/main.go
@@ -2,21 +2,31 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/Sirupsen/logrus"
 	securedropbot "github.com/tmc/securedrop-bot"
 )
 
+var flagVerbose = flag.Bool("v", false, "be verbose")
+
 func main() {
+	flag.Parse()
+	logger := logrus.New()
+	if *flagVerbose {
+		logger.SetLevel(logrus.DebugLevel)
+		logrus.SetLevel(logrus.DebugLevel)
+	}
 	ctx := context.Background()
 	port := "8001"
 	if p := os.Getenv("PORT"); p != "" {
 		port = p
 	}
-	h, err := securedropbot.NewHandler(ctx)
+	h, err := securedropbot.NewHandler(ctx, logger)
 	if err != nil {
 		log.Fatal(err)
 	}
