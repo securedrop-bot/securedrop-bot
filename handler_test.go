68d4009ec76862d2c22e9cf9a349adbf86849970
redshiftzero <jen@freedom.press>
2018-01-14 20:26:55 -0800

    wip

--- handler_test.go
+++ handler_test.go
@@ -9,13 +9,22 @@ import (
 	"github.com/google/go-github/github"
 )
 
-var failureStatus = "failure"
-var botLogin = "securedrop-bot"
-var fiveMinutesAgo = time.Now().Add(-5 * time.Minute)
+var (
+	failureStatus   = "failure"
+	botLogin        = "securedrop-bot"
+	fiveMinutesAgo  = time.Now().Add(-5 * time.Minute)
+	threeDaysAgo    = time.Now().Add(-72 * time.Hour)
+	approvedStatus  = "APPROVED"
+	genericPRNumber = 1
+	userLogin       = "tmc"
+)
 
 var (
 	botUser = &github.User{
 		Login: &botLogin,
+	}
+	reviewerUser = &github.User{
+		Login: &userLogin,
 	}
 	failureCombinedStatus = &github.CombinedStatus{
 		State: &failureStatus,
@@ -30,6 +39,16 @@ var (
 			User:      botUser,
 			CreatedAt: &fiveMinutesAgo,
 		},
+	}
+	approvedReviews = []*github.PullRequestReview{
+		&github.PullRequestReview{
+			State:       &approvedStatus,
+			User:        reviewerUser,
+			SubmittedAt: &threeDaysAgo,
+		},
+	}
+	genericPullRequest = &github.PullRequest{
+		Number: &genericPRNumber,
 	}
 )
 
@@ -54,6 +73,16 @@ var (
 		},
 		ListCommentsFunc: func(in1 context.Context, in2 string, in3 string, in4 int, in5 *github.IssueListCommentsOptions) ([]*github.IssueComment, *github.Response, error) {
 			return recentBotComments, nil, nil
+		},
+	}
+	prIsApproved = &pullRequestsServiceMock{
+		ListReviewsFunc: func(in1 context.Context, in2 string, in3 string, in4 int, in5 *github.ListOptions) ([]*github.PullRequestReview, *github.Response, error) {
+			return approvedReviews, nil, nil
+		},
+	}
+	prIsNotApproved = &pullRequestsServiceMock{
+		ListReviewsFunc: func(in1 context.Context, in2 string, in3 string, in4 int, in5 *github.ListOptions) ([]*github.PullRequestReview, *github.Response, error) {
+			return []*github.PullRequestReview{}, nil, nil
 		},
 	}
 )
@@ -99,6 +128,56 @@ func TestHandler_nagSubmitterIfFailed(t *testing.T) {
 			got := len(tt.client.issues.calls.CreateComment)
 			if got != tt.nComments {
 				t.Errorf("Handler.nagSubmitterIfFailed, got %d comments; want %d", got, tt.nComments)
+			}
+		})
+	}
+}
+
+func TestHandler_nagMaintainerForMerge(t *testing.T) {
+	logger := logrus.New()
+	if testing.Verbose() {
+		logger.SetLevel(logrus.DebugLevel)
+	}
+
+	ctx := context.Background()
+	type args struct {
+		ctx context.Context
+		pr  *github.PullRequest
+	}
+	tests := []struct {
+		name      string
+		client    *fakeGithubClient
+		args      args
+		nComments int // how many comments we expect to be posted
+		wantErr   bool
+	}{
+		{"pr-is-not-approved", &fakeGithubClient{
+			repositories: failingStatusRepository,
+			pullRequests: prIsNotApproved,
+			issues:       noRecentComments,
+		}, args{ctx: ctx, pr: genericPullRequest}, 0, false},
+		{"pr-is-approved-no-comments", &fakeGithubClient{
+			pullRequests: prIsApproved,
+			issues:       noRecentComments,
+		}, args{ctx: ctx, pr: genericPullRequest}, 1, false},
+		{"pr-is-approved-bot-already-commented", &fakeGithubClient{
+			repositories: failingStatusRepository,
+			pullRequests: prIsApproved,
+			issues:       botRecentlyCommentedIssues,
+		}, args{ctx: ctx, pr: genericPullRequest}, 0, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := &Handler{
+				logger: logger,
+				client: tt.client,
+			}
+			if err := h.nagMaintainerForMerge(tt.args.ctx, tt.args.pr); (err != nil) != tt.wantErr {
+				t.Errorf("Handler.nagMaintainerForMerge() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			got := len(tt.client.issues.calls.CreateComment)
+			if got != tt.nComments {
+				t.Errorf("Handler.nagMaintainerForMerge, got %d comments; want %d", got, tt.nComments)
 			}
 		})
 	}
